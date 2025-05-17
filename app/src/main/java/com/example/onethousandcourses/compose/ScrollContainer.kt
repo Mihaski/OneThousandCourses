f953e8410038c6867b39906b5c4baf43cbd009e3
Mikhail Zabrodin <mzabrodin94@gmail.com>
2025-05-17 17:56:31 +0700

    initial commit

---
+++ app/src/main/java/com/example/onethousandcourses/compose/ScrollContainer.kt
@@ -0,0 +1,30 @@
+package com.example.onethousandcourses.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(widthDp = 200, heightDp = 200)
+@Composable
+fun ScrollContainer() {
+
+    Column(
+        Modifier
+            .background(Color.LightGray)
+            .size(100.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        repeat(10) {
+            Text("Item $it", Modifier.padding(2.dp))
+        }
+    }
+}
