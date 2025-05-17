a3e68b19084df8b08070ab448e8c17a77c095653
Mikhail Zabrodin <mzabrodin94@gmail.com>
2025-05-17 20:22:23 +0700

    описал container

--- app/src/main/java/com/example/onethousandcourses/compose/ScrollContainer.kt
+++ app/src/main/java/com/example/onethousandcourses/compose/ScrollContainer.kt
@@ -1,30 +1,24 @@
 package com.example.onethousandcourses.compose
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.onethousandcourses.R
 
 @Preview(widthDp = 200, heightDp = 200)
 @Composable
-fun ScrollContainer() {
+fun ScrollContainer(painter: Painter = painterResource(R.drawable.courses_preview)) {
 
-    Column(
+    Box(
         Modifier
-            .background(Color.LightGray)
-            .size(100.dp)
-            .verticalScroll(rememberScrollState())
+            .horizontalScroll(rememberScrollState())
     ) {
-        repeat(10) {
-            Text("Item $it", Modifier.padding(2.dp))
-        }
+        Image(painter, "Картинка скролящаяся горизонтально")
     }
 }
